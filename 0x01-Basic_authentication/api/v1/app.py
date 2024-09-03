340f2cb7521db5b0f93b79d89c213560f10aa224
omo <omoikheloa56@gmail.com>
2024-09-03 23:16:42 +0000

    auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from typing_extensions import Literal
 
 
 app = Flask(__name__)
@@ -19,6 +20,13 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> tuple[str, Literal[401]]:
+    """ Not found handler
+    """
+    return jsonify({"error": "unauthorized"}), 401
 
 
 if __name__ == "__main__":
