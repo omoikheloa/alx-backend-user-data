31f6b8024d7dc7c503c641fefe3c2e1432a39146
omo <omoikheloa56@gmail.com>
2024-09-04 00:21:56 +0000

    bew changes to app

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from typing import Tuple
 
 
 app = Flask(__name__)
@@ -23,8 +22,9 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> Tuple[str, int]:
-    """ Unauthorized handler
+def unauthorized(error) -> str:
+    """
+    Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
